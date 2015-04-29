65b4f23de929aaf5eb58fc67fd2b00cec72ecea3
VictorWolmeister <wolmeistervictor@gmail.com>
2015-04-28 22:42:29 -0300

    Beta 1.02

--- o.java
+++ o.java
@@ -1,5 +1,5 @@
-//Created by Victor Wolmeister - 25/04/2015
-//1.01
+//Created by Victor Wolmeister - 28/04/2015
+//1.02
 import java.util.Scanner;
 import java.util.Random;
 public class o
@@ -17,20 +17,21 @@ public class o
   static boolean foi=false;
   static int X=0;
   static int O=0;
+  static String P1,P2;
+  static int Starter; //Variavel para decidir quem começa
   public static void main(String args[])
   {
-    Random random = new Random();
     Scanner leia = new Scanner(System.in);
     String cord ="";
-    char P1,P2;
-    P1 = 'X'; //Par
-    P2 = 'O'; //Impar
-    md = melhorD();////////////////////
+    start();
+    getNick();
+    md = melhorD();
     while(X!=md && O!=md)
     {
+      randomStart();
       while(verifEnd()==false && vez!=9)
       {
-        System.out.println("Digite a coordenada");
+        System.out.println("Digite a coordenada que deseja, "+whoisNow());
         while(foi==false)
         {
           cord=leia.nextLine();
@@ -39,45 +40,30 @@ public class o
             foi=true;
           }
         }
-        if(vez%2==0)
+        if(Starter%2==0) //par = x
         {
           tab[DrawSlot(cord)-1]='X';
           drawedX[ixDrawed]=DrawSlot(cord);
           ixDrawed++;  
         }
-        else
+        else //impar = o
         {
           tab[DrawSlot(cord)-1]='O';
           drawedO[ioDrawed]=DrawSlot(cord);
           ioDrawed++;
         }
         vez++;
+        Starter++;
         gone[igone]=DrawSlot(cord);
         igone++;
         DrawTable();
         foi=false;
       }  
-      //Verifica se houve empate,X ou Y ganhou
-      if(verifEnd()==true)
-      {
-        System.out.println(verifWinner() + " ganhou!");
-        if(verifWinner().equals("X"))
-        {
-          X++;
-        }
-        else
-        {
-          O++;
-        }
-      }
-      else
-      {
-        System.out.println("Houve empate");
-      }
+      scoreMatch();
       placar();
-      reset();///////////////////////
+      reset();
     }
-    
+    winner();
   }
   
   public static void DrawTable()
@@ -266,7 +252,7 @@ public class o
   {
     Scanner leia = new Scanner(System.in);
     int k;
-    System.out.println("Voce deseja jogar uma melhor de 1,3 ou 5 partidas?");
+    System.out.println("Voces desejam jogar uma melhor de 1,3 ou 5 partidas?");
     do
     {
       System.out.println("Digite 1 ou 3 ou 5");
@@ -288,5 +274,91 @@ public class o
     System.out.println("     PLACAR");
     System.out.println("  X          O");
     System.out.println("  "+X+"          "+O);
+  }
+  
+  public static void getNick()
+  {
+    Scanner leia = new Scanner(System.in);
+    System.out.println("Jogador 1, por favor, digite seu nick");
+    P1 = leia.nextLine();
+    System.out.println("Jogador 2, por favor, digite seu nick");
+    P2 = leia.nextLine();
+    System.out.println("Nicks registrados com sucesso! "+P1+" voce será o X, e "+P2+" voce sera o O");
+  }
+  
+  public static void scoreMatch()
+  {
+    if(verifEnd()==true)
+    {
+      if(verifWinner().equals("X"))
+      {
+        System.out.println("Parabens "+P1+" voce ganhou um ponto!");
+        X++;
+      }
+      else
+      {
+        System.out.println("Parabens "+P2+" voce ganhou um ponto!");
+        O++;
+      }
+    }
+    else
+    {
+      System.out.println("É... "+P1 + " e "+P2+" empataram e ninguem ganhou ponto");
+    }
+  }
+  
+  public static void randomStart()
+  {
+    Random random = new Random();
+    Starter = random.nextInt(100);
+    if(Starter % 2 == 0)
+    {
+      System.out.println("É com voce "+P1+", pode começar!");
+    }
+    else
+    {
+      System.out.println("É com voce "+P2+", pode começar!");
+    }
+  }
+  
+  public static String whoisNow()
+  {
+    if(Starter%2==0)
+    {
+      return P1;
+    }
+    else
+    {
+      return P2;
+    }
+  }
+  
+  public static void start()
+  {
+    System.out.println("Olá, sejam bem vindos aos Jogo da Velha");
+    System.out.println("O objetivo do jogo é preencher uma coluna,linha ou diagonal da tabela 3x3 utilizando o mesmo simbolo");
+    System.out.println("Cada jogador pode adotar uma estrategia, seja para vencer ou apenas para se defender e levar ao empate");
+    System.out.println("É necessario 2 jogadores para poder jogar");
+    System.out.println("Sera escolhido aleatoriamente quem ira começar a partida");
+    System.out.println("Nesse jogo, para selecionar o lugar que deseja colocar o simbolo, usaremos um sistema de coordenadas, como o mostrado a seguir");
+    System.out.println("[1,1][2,1][3,1]");
+    System.out.println("[1,2][2,2][3,2]");
+    System.out.println("[1,3][2,3][3,3]");
+    System.out.println("Ou seja, quando for sua vez de jogar, basta digitar a coordenada que deseja, por exemplo 2,2.");
+    System.out.println("Não se preocupe, caso digite errado o jogo ira te avisar ;)");
+    System.out.println("Um jogador sera representado pelo simbolo X e o outro pelo simbolo O");
+    System.out.println("Então vamos começar!");
+  }
+  
+  public static void winner()
+  {
+    if(verifWinner().equals("X"))
+    {
+      System.out.println("!!!PARABENS "+P1+" VOCE GANHOU O JOGO!!!");
+    }
+    else
+    {
+      System.out.println("!!!PARABENS "+P2+" VOCE GANHOU O JOGO!!!");
+    }
   }
 }
